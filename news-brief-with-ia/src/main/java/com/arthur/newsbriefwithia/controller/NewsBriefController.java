f24bdcfd5bfe63159243344caecdfa8b55ba63d6
arthurd3 <arthur_camposl@yahoo.com>
2025-08-31 11:27:46 -0300

    feat: create classes to connect a ollama

--- news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/controller/NewsBriefController.java
+++ news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/controller/NewsBriefController.java
@@ -3,11 +3,11 @@ package com.arthur.newsbriefwithia.controller;
 import com.arthur.newsbriefwithia.dto.NewsSummaryResponse;
 import com.arthur.newsbriefwithia.service.NewsBriefService;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.print.attribute.standard.Media;
 import java.awt.*;
 
 @RestController
@@ -26,8 +26,16 @@ public class NewsBriefController {
     }
 
     @GetMapping(value = "/general-brief/render" , produces = MediaType.TEXT_HTML_VALUE)
-    public NewsSummaryResponse generalBriefUI(){
-        return newsBriefService.generateGeneralNewsBrief(true);
+    public ResponseEntity<String> generalBriefUI(){
+        final NewsSummaryResponse newsSummaryResponse =
+                newsBriefService.generateGeneralNewsBrief(true);
+
+        final String htmlContent = newsSummaryResponse
+                .getSummary().substring(7 , newsSummaryResponse.getSummary().length() - 3);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(htmlContent);
     }
 
 
