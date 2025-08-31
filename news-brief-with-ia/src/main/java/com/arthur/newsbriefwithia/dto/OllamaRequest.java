3f73073d0a7def2445a1d4b7936d27f744ba7faa
arthurd3 <arthur_camposl@yahoo.com>
2025-08-31 10:55:01 -0300

    feat: create classes to connect a ollama

--- news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/dto/OllamaRequest.java
+++ news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/dto/OllamaRequest.java
@@ -10,4 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OllamaRequest {
+    private String model;
+    private String prompt;
+    private boolean stream;
 }
