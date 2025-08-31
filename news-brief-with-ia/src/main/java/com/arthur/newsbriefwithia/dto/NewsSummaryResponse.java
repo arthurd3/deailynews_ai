f26c473170d869ee5b0fa19fc78698f0a2123b75
arthurd3 <arthur_camposl@yahoo.com>
2025-08-30 23:07:17 -0300

    feat: create classes

--- news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/dto/NewsSummaryResponse.java
+++ news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/dto/NewsSummaryResponse.java
@@ -1,5 +1,20 @@
 package com.arthur.newsbriefwithia.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class NewsSummaryResponse {
+
+    private String summary;
+    private LocalDate createdAt;
+
+
 }
--- news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/service/NewsBriefService.java
+++ news-brief-with-ia/src/main/java/com/arthur/newsbriefwithia/service/NewsBriefService.java
@@ -1,14 +1,29 @@
 package com.arthur.newsbriefwithia.service;
 
+import com.arthur.newsbriefwithia.client.NewsApiClient;
+import com.arthur.newsbriefwithia.dto.NewsApiResponse;
 import com.arthur.newsbriefwithia.dto.NewsSummaryResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @Slf4j
 public class NewsBriefService {
 
+    private final NewsApiClient newsApiClient;
+
+    public NewsBriefService(NewsApiClient newsApiClient) {
+        this.newsApiClient = newsApiClient;
+    }
+
     public NewsSummaryResponse generateGeneralNewsBrief() {
         final NewsApiResponse newsApiResponse = newsApiClient.getTopHeadlines();
+        log.info("Top Headlines: {}", newsApiResponse);
+        return NewsSummaryResponse.builder()
+                .createdAt(LocalDate.now())
+                .summary("This placehoard")
+                .build();
     }
 }
